a2dd5e2819c7c228b4e18030a6a151723011ca43
Anton Keks <anton@azib.net>
2026-01-19 18:24:41 +0200

    prerender the page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 import {loadAlbums} from '$lib/album_loader'
 import type {PageServerLoad} from './$types'
 
+export const prerender = true;
+
 export const load: PageServerLoad = async () => {
   const albums = loadAlbums()
   return {
